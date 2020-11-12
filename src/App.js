d1e0ba19d29f189e7c4bdc37ff95637da295580c
wilmerbaby <wilmerbaby@gmail.com>
2020-11-12 05:04:03 -0500

    rebilt app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import AboutContainer from "./container/AboutContainer/AboutContainer"
- import Portfolio from "./container/Portfolio/Portfolio.jsx"
- import{useEffect} from "react";
-
-  // Setting this.state.friends to the friends json array
-
+// import AboutContainer from "./container/AboutContainer/AboutContainer"
+//  import Portfolio from "./container/Portfolio/Portfolio.jsx"
+ import React from "react";
+ import {BrowserRouter as Router, Rout, Link } from 'react-router-dom';
+ import container from 'react-bootsrap/container';
  
- const App = () => {
-   return (
-    <Portfolio/>
-   );
- };
- 
+ class App extends React.Component{
+   constructor(props){
+     super(props);
+     this.state= {
+       title: 'Wilmer Rivera',
+       headerLinks: [
+         { title: 'about', path: '/'},
+         { title: 'portfolio', path: '/portfolio'}
+       ],
+       home: {
+         tiltle: 'Taking steps',
+         subTitle: 'projects time',
+         text : 'look what I created'
+       },
+       about: {
+         title: 'Learn about me'
+       },
+       contact: {
+         title: 'pick up the phone'
+       }
+
+     }
+     
+   }
+   render() {
+     return(
+<Router>
+      <h1>sup</h1>
+      </Router>
+     );
+   }
+ }
  export default App;
--- src/components/Project/ProjectCard.jsx
+++ src/components/Project/ProjectCard.jsx
@@ -2,29 +2,31 @@ import React from "react";
 import "./style.css";
 
 
+import React from 'react';
+
 const ProjectCard = () => {
   return (
     <div className="card">
-      <div className="img-container">
-        <img alt="{props.name}" src="{props.image} "/>
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> wilmer
-          </li>
-          <li>
-            <strong>Occupation:</strong> coding
-          </li>
-          <li>
-            <strong>Location:</strong> rome
-          </li>
-        </ul>
-      </div>
-      <span onClick={() => props.removeFriend(props.id)} className="remove">
-        𝘅
-      </span>
+    <div className="img-container">
+      <img alt="{props.name}" src="{props.image} "/>
     </div>
+    <div className="content">
+      <ul>
+        <li>
+          <strong>Name:</strong> wilmer
+        </li>
+        <li>
+          <strong>Occupation:</strong> coding
+        </li>
+        <li>
+          <strong>Location:</strong> rome
+        </li>
+      </ul>
+    </div>
+    <span onClick="{() => props.removeFriend(props.id)} "className="remove">
+      𝘅
+    </span>
+  </div>
   );
 };
 
