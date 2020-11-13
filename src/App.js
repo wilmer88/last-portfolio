b65121009a25ff7e27503a2566f6f5cae4cd333f
wilmerbaby <wilmerbaby@gmail.com>
2020-11-12 19:32:45 -0500

    router or switch not working. worked on it for 3hr no luck

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
-// import AboutContainer from "./container/AboutContainer/AboutContainer"
-//  import Portfolio from "./container/Portfolio/Portfolio.jsx"
- import React from "react";
- import {BrowserRouter as Router, Rout, Link } from 'react-router-dom';
- import container from 'react-bootsrap/container';
- 
- class App extends React.Component{
-   constructor(props){
-     super(props);
-     this.state= {
-       title: 'Wilmer Rivera',
-       headerLinks: [
-         { title: 'about', path: '/'},
-         { title: 'portfolio', path: '/portfolio'}
-       ],
-       home: {
-         tiltle: 'Taking steps',
-         subTitle: 'projects time',
-         text : 'look what I created'
-       },
-       about: {
-         title: 'Learn about me'
-       },
-       contact: {
-         title: 'pick up the phone'
-       }
 
-     }
-     
-   }
-   render() {
-     return(
-<Router>
-      <h1>sup</h1>
-      </Router>
-     );
-   }
- }
- export default App;
+import Projects from "./components/AboutContainer/Projects"
+import AboutContainer from "./components/AboutContainer/AboutContainer";
+// import portfolio from "./pages/portfolio";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+
+function App() {
+  return (
+    <>
+   <Router>
+ <Switch>
+ <Route  path="/portfolio" component={Projects}/>
+ <Route  path="/" component={AboutContainer}/>
+
+ </Switch>
+    </Router>
+  
+   </>
+  );
+}
+
+export default App;
+
+
--- src/components/Wrapper/wraper.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./wraper.css";
-
-function Wrapper() {
-  return <div className="wrapper"></div>;
-}
-
-export default Wrapper;
